3dced2769a387906f36a3bb1b385ffeb382f4b75
marcellacg <marcelladacosta@gmail.com>
2023-05-16 20:34:00 -0300

    att

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from remote_object import RemoteObject
 
 remote_obj = RemoteObject()
 
-daemon = Pyro4.Daemon()
+daemon = Pyro4.Daemon(host='10.10.132.234', port=9090)
 uri = daemon.register(remote_obj)
 
 print("URI do objeto remoto:", uri)
