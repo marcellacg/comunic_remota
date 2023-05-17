16d43b75f1ac818ee080f7c3981166f2dd042fe6
marcellacg <marcelladacosta@gmail.com>
2023-05-17 20:05:00 -0300

    att2

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from remote_object import RemoteObject
 
 remote_obj = RemoteObject()
 
-daemon = Pyro4.Daemon(host='192.168.36.142', port=18861)
+#Utilizar IP Wifi (testado em Ubuntu em diferentes redes, porém, resultando em time out)
+daemon = Pyro4.Daemon(host='192.168.36.138', port=9090)
 uri = daemon.register(remote_obj)
 
 print("URI do objeto remoto:", uri)
